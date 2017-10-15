b0ce725adff1de8e55b0c70f51e2d52121d1a592
Yaxi Ye <ss1271@gmail.com>
2017-10-15 18:10:54 +0100

    update for ApiInterface and User model

--- app/src/main/java/com/yeyaxi/android/playground/ApiInterface.java
+++ app/src/main/java/com/yeyaxi/android/playground/ApiInterface.java
@@ -9,6 +9,8 @@ import java.util.List;
 
 import io.reactivex.Observable;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 public interface ApiInterface {
     String BASE_URL = "http://jsonplaceholder.typicode.com/";
@@ -16,10 +18,16 @@ public interface ApiInterface {
     @GET("posts")
     Observable<List<Post>> getPosts();
 
+    @GET("posts/{id}")
+    Observable<Post> getPost(@Path("id") Long postId);
+
     @GET("users")
     Observable<List<User>> getUsers();
 
-    @GET("comments")
-    Observable<List<Comment>> getComments();
+    @GET("users/{id}")
+    Observable<User> getUser(@Path("id") Long id);
+
+    @GET("comments/")
+    Observable<List<Comment>> getComments(@Query("postId") Long postId);
 
 }
